b8a8f2f2c5406457759759055347debc822185e7
sabbirahmedfahim <dev.sabbirfahim@gmail.com>
2025-04-22 21:44:36 +0600

    slightly improved

--- WEEK_02/module_6/3_dijkstra_optimized.cpp
+++ WEEK_02/module_6/3_dijkstra_optimized.cpp
@@ -18,18 +18,19 @@ using namespace std;
 const int N = 1E4;
 vector<pair<int, int>> v[N]; // Node and weight (cost) pair
 int dis[N]; /* dis == cost */
-
+// int parent[N]; // track parent node
 void dijkstra(int src) // O(logV(V+E)) -> O(VlogV + ElogV) -> O(VlogV + ElogE)
 {
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; // Min-heap by default
     pq.push({src, 0});
     dis[src] = 0;
+    // parent[src] = -1; // track parent node
     while (!pq.empty())
     {
-        pair<int, int> parent = pq.top();
+        pair<int, int> par = pq.top();
         pq.pop();
-        int node = parent.first;  // or nodeVal or parentNode
-        int cost = parent.second; // or parentCost
+        int node = par.first;  // or nodeVal or parentNode
+        int cost = par.second; // or parentCost
         for (pair<int, int> child : v[node]) // range based for loop
         {
             int childNode = child.first;
@@ -38,6 +39,7 @@ void dijkstra(int src) // O(logV(V+E)) -> O(VlogV + ElogV) -> O(VlogV + ElogE)
             {
                 // path relax
                 dis[childNode] = cost + childCost;
+                // parent[childNode] = node; // track parent node
                 pq.push({childNode, dis[childNode]}); // dis[childNode] bola r (cost+childCost) bola ekh kotha
             }
         }
@@ -56,6 +58,7 @@ int main()
     }
     
     memset(dis, 999999, sizeof(dis));
+    // memset(parent, -1, sizeof(parent)); // parent tracking
 
     int src, des; cin >> src >> des;
     dijkstra(src);
