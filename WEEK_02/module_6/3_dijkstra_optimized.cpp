05f7473501fabf0923ee4a4d584b11bccee7f5c1
sabbirahmedfahim <dev.sabbirfahim@gmail.com>
2025-04-23 22:39:03 +0600

    modified inputs

--- WEEK_02/module_6/3_dijkstra_optimized.cpp
+++ WEEK_02/module_6/3_dijkstra_optimized.cpp
@@ -4,14 +4,14 @@ Input:
 0 1 10
 0 2 7
 0 3 4
-1 4 3
+1 4 2
 2 4 5
 2 1 1
 3 4 5
 3 2 1
 0 4
-Output:
-9
+Expected Output:
+8
 */
 /*
 Please note that in the priority queue, we'll push {cost, source} pair to ensure proper
